32fc6abbe948f5aae1a402c495f3ad083325e1db
Ying Wang <xingqiwang@gmail.com>
2026-09-01 15:40:32 +0200

    chore: bump versionCode to 11 (v2.2.1) for Play Store release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yingwang.chinesechess"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "2.2.0"
+        versionCode = 11
+        versionName = "2.2.1"
     }
 
     signingConfigs {
